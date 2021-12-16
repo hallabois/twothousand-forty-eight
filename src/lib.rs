840489749390cea7697a5da690ef875c284c5fdf
Elias Eskelinen <elias.eskelinen@pm.me>
2021-12-16 22:11:12 +0200

    add possibility to add random tiles

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ pub const DEBUG_INFO: bool = false;
 
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
+#[cfg(feature = "wasm")]
+use rand::prelude::*;
 
 #[cfg(feature = "wasm")]
 #[wasm_bindgen]
@@ -39,10 +41,22 @@ pub fn validate(data: &str) -> String {
 
 #[cfg(feature = "wasm")]
 #[wasm_bindgen]
-pub fn is_move_possible(board_data: &str, dir: usize) -> String {
+pub fn apply_move(board_data: &str, dir: usize, add_random: bool) -> String {
     let b = serde_json::from_str(board_data).unwrap();
     // let first_move_valid = validator::validate_first_move(&parsed);
-    let result = board::is_move_possible(b, direction::Direction::from_index(dir));
+    let mut result = board::is_move_possible(b, direction::Direction::from_index(dir));
+    if add_random {
+        let mut game = board::Board{ width: b.width, height: b.height, tiles: result.0 };
+        let mut possible = game.get_non_occupied_tiles();
+        if possible.len() > 0 {
+            possible.shuffle(&mut rand::thread_rng());
+            let t = possible[0];
+            let mut possible_values = vec![2, 2, 2, 4];
+            possible_values.shuffle(&mut rand::thread_rng());
+            game.set_tile(t.x, t.y, possible_values[0]);
+        }
+        result.0 = game.tiles;
+    }
     return serde_json::to_string(&result).unwrap();
 }
 
