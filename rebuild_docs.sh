3d430d8d1ed7b04aa384e68432e365d31d2ad31e
Elias Eskelinen <elias.eskelinen@pm.me>
2022-03-07 13:38:50 +0200

    Rebuild docs

--- rebuild_docs.sh
+++ rebuild_docs.sh
@@ -1,4 +1,4 @@
-cargo doc --no-deps
+cargo doc --release --no-deps
 rm -rf ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=twothousand_forty_eight\">" > target/doc/index.html
 cp -r target/doc ./docs
