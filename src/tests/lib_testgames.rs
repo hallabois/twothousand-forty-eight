eb50ce16074f0875682f10c6f97da6eba59f58c1
Elias Eskelinen <elias.eskelinen@pm.me>
2022-03-07 10:33:31 +0200

    separate tests from the lib.rs file

---
+++ src/tests/lib_testgames.rs
@@ -0,0 +1,6 @@
+pub const GAME3X3: &str = include_str!("../../games/GAME3X3A.txt");
+pub const GAME3X3B: &str = include_str!("../../games/GAME3X3B.txt");
+pub const GAME4X4: &str = include_str!("../../games/GAME4X4.txt");
+pub const GAME4X4B: &str = include_str!("../../games/GAME4X4B.txt");
+pub const GAME4X4C: &str = include_str!("../../games/GAME4X4C.txt");
+pub const GAMEOBSCENE: &str = include_str!("../../games/GAME4X4BIG.txt");
