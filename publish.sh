9e736fdfd38e84a8f6470afc4afaeed6322dc9ac
Elias Eskelinen <pgp@eliaseskelinen.fi>
2022-09-23 11:32:16 +0300

    disable wasm on the default feature-set

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 echo "Publishing to crates.io..."
 cargo publish
 echo "Publishing to npm..."
-wasm-pack build --release
+wasm-pack build --release --features wasm
 node patch_wasm.js
 wasm-pack pack
 wasm-pack publish
